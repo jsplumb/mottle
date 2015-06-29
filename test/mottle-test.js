11319079c72119017cf98a66788cdf81ae0867f1
sporritt <simon.porritt@gmail.com>
2015-06-29 22:14:05 +1000

    add tests for focus/blur

--- test/mottle-test.js
+++ test/mottle-test.js
@@ -1,204 +1,228 @@
-var divs  =[],
-	_add = function(id, className) {
-		var d = document.createElement("div");
-		d.setAttribute("id", id);
-		if (className) d.className = className;
-		divs.push(d);
-		document.body.appendChild(d);
-		return d;
-	},
-	_clear = function() {
-		for (var i = 0; i < divs.length; i++) {
-			try {
-				divs[i].parentNode.removeChild(divs[i]);
-			}
-			catch (e) { }
-		}
-		divs.length = 0;
-		//m.reset();
-	},
-	m;
+var divs = [],
+    _add = function (id, className) {
+        var d = document.createElement("div");
+        d.setAttribute("id", id);
+        if (className) d.className = className;
+        divs.push(d);
+        document.body.appendChild(d);
+        return d;
+    },
+    _clear = function () {
+        for (var i = 0; i < divs.length; i++) {
+            try {
+                divs[i].parentNode.removeChild(divs[i]);
+            }
+            catch (e) {
+            }
+        }
+        divs.length = 0;
+        //m.reset();
+    },
+    m;
 
-var testSuite = function() {
-	
-	module("Mottle", {
-		teardown: _clear,
-		setup:function() {
+var testSuite = function () {
+
+    module("Mottle", {
+        teardown: _clear,
+        setup: function () {
             m = new Mottle();
-		}
-	});
-
-    test("bind a click on document and trigger one, no original event", function() {
-        var a = null;
-		m.on(document, "click", function() { a = "done"; });
-		m.trigger(document, "click");
-		equal(a, "done", "click event was registered and triggered");
+        }
     });
 
-    test("event delegation: bind a click on document with div filters and trigger one, no original event", function() {
-    	var d1 = _add("d1", "foo");
-    	var d2 = _add("d2", "foo");
-    	var d3 = _add("d3", "bar");
+    test("bind a click on document and trigger one, no original event", function () {
         var a = null;
-		m.on(document, "click", ".foo", function() { a = "done"; });
-		m.trigger(d2, "click");
-		equal(a, "done", "click event was registered and triggered");
-		a = null;
-		m.trigger(d3, "click");
-		equal(a, null, "click event did not fire for unregistered selector");
+        m.on(document, "click", function () {
+            a = "done";
+        });
+        m.trigger(document, "click");
+        equal(a, "done", "click event was registered and triggered");
     });
-    
-    test("bind a click and trigger one, no original event", function() {
+
+    test("event delegation: bind a click on document with div filters and trigger one, no original event", function () {
+        var d1 = _add("d1", "foo");
+        var d2 = _add("d2", "foo");
+        var d3 = _add("d3", "bar");
+        var a = null;
+        m.on(document, "click", ".foo", function () {
+            a = "done";
+        });
+        m.trigger(d2, "click");
+        equal(a, "done", "click event was registered and triggered");
+        a = null;
+        m.trigger(d3, "click");
+        equal(a, null, "click event did not fire for unregistered selector");
+    });
+
+    test("bind a click and trigger one, no original event", function () {
         var d = _add("d1");
-		var a = null;
-		m.on(d, "click", function() { a = "done"; });
-		m.trigger(d, "click");
-		equal(a, "done", "click event was registered and triggered");
+        var a = null;
+        m.on(d, "click", function () {
+            a = "done";
+        });
+        m.trigger(d, "click");
+        equal(a, "done", "click event was registered and triggered");
     });
-	
-	test("bind a click, two elements in array, and trigger one, no original event", function() {
+
+    test("bind a click, two elements in array, and trigger one, no original event", function () {
         var d = _add("d1"), d2 = _add("d2");
-		var a = null;
-		m.on([d, d2], "click", function() { a = "done"; });
-		m.trigger(d, "click");
-		equal(a, "done", "click event was registered and triggered");
-		a = null;
-		m.trigger(d2, "click");
-		equal(a, "done", "click event was registered and triggered");
+        var a = null;
+        m.on([d, d2], "click", function () {
+            a = "done";
+        });
+        m.trigger(d, "click");
+        equal(a, "done", "click event was registered and triggered");
+        a = null;
+        m.trigger(d2, "click");
+        equal(a, "done", "click event was registered and triggered");
     });
-	
-	test("bind a click by id, trigger event, no original event", function() {
+
+    test("bind a click by id, trigger event, no original event", function () {
         var d = _add("d1");
-		var a = null;
-		m.on("#d1", "click", function() { a = "done"; });
-		m.trigger("#d1", "click");
-		equal(a, "done", "click event was registered and triggered");
+        var a = null;
+        m.on("#d1", "click", function () {
+            a = "done";
+        });
+        m.trigger("#d1", "click");
+        equal(a, "done", "click event was registered and triggered");
     });
-	
-	test("trigger by selector", function() {
+
+    test("trigger by selector", function () {
         var d = _add("d1"), d2 = _add("d2");
-		d.className = "foo";
-		d2.className = "foo";
-		var a = 0;
-		m.on([d, d2], "click", function() { a++; });
-		m.trigger(document.querySelectorAll(".foo"), "click");
-		equal(a, 2, "click event was registered and triggered on both elements");
+        d.className = "foo";
+        d2.className = "foo";
+        var a = 0;
+        m.on([d, d2], "click", function () {
+            a++;
+        });
+        m.trigger(document.querySelectorAll(".foo"), "click");
+        equal(a, 2, "click event was registered and triggered on both elements");
     });
-	
-	test("bind and trigger by selector", function() {
+
+    test("bind and trigger by selector", function () {
         var d = _add("d1"), d2 = _add("d2");
-		d.className = "foo";
-		d2.className = "foo";
-		var a = 0, fooDivs = document.querySelectorAll(".foo");
-		m.on(fooDivs, "click", function() { a++; });
-		m.trigger(fooDivs, "click");
-		equal(a, 2, "click event was registered and triggered on both elements");
+        d.className = "foo";
+        d2.className = "foo";
+        var a = 0, fooDivs = document.querySelectorAll(".foo");
+        m.on(fooDivs, "click", function () {
+            a++;
+        });
+        m.trigger(fooDivs, "click");
+        equal(a, 2, "click event was registered and triggered on both elements");
     });
-	
-	test("bind a click and delete the element, then unbind", function() {
+
+    test("bind a click and delete the element, then unbind", function () {
         var d = _add("d1");
-		var a = 0;
-		var f = function() { a++; };
-		m.on(d, "click", f);
-		m.trigger(d, "click");
-		equal(a, 1, "event was fired");
-		m.trigger(d, "click");
-		equal(a, 2, "event was fired again");
-		m.off(d, "click", f);
-		m.trigger(d, "click");
-		equal(a, 2, "event was not fired again after unbind");
-		
+        var a = 0;
+        var f = function () {
+            a++;
+        };
+        m.on(d, "click", f);
+        m.trigger(d, "click");
+        equal(a, 1, "event was fired");
+        m.trigger(d, "click");
+        equal(a, 2, "event was fired again");
+        m.off(d, "click", f);
+        m.trigger(d, "click");
+        equal(a, 2, "event was not fired again after unbind");
+
     });
 
     /**
      * Tests that unbinding from a deleted element does not fail.
      * @method Mottle.Test.UnbindFromDeletedElement
      */
-	test("bind a click and delete the element, then unbind", function() {
+    test("bind a click and delete the element, then unbind", function () {
         var d = _add("d1");
-		var a = null;
-		var f = function() { a = "done"; };
-		m.on(d, "click", f);
-		d.parentNode.removeChild(d);
-		try {
-			// get element; it has been removed and is now null.
-			// i could of course just pass null in here ....
-			var dd = document.getElementById("d1");
-			m.off(dd, "click", f);
-			ok(true, "unbind should not throw an error");
-		}
-		catch (e) {
-			ok(false, "unbind should not throw an error");
-		}
+        var a = null;
+        var f = function () {
+            a = "done";
+        };
+        m.on(d, "click", f);
+        d.parentNode.removeChild(d);
+        try {
+            // get element; it has been removed and is now null.
+            // i could of course just pass null in here ....
+            var dd = document.getElementById("d1");
+            m.off(dd, "click", f);
+            ok(true, "unbind should not throw an error");
+        }
+        catch (e) {
+            ok(false, "unbind should not throw an error");
+        }
     });
 
     /**
      * Tests that binding to a null element does not fail.
      * @method Mottle.Test.BindNullElement
      */
-	test("bind to a null element", function() {
-		expect(1);
-		m.on(null, "dblclick", function() {
-			
-		});
-		ok(true, "bind to null did not fail");
-	});
+    test("bind to a null element", function () {
+        expect(1);
+        m.on(null, "dblclick", function () {
+
+        });
+        ok(true, "bind to null did not fail");
+    });
 
     /**
      * Tests that binding a null function does not fail.
      * @method Mottle.Test.BindNull
      */
-	test("bind a null function", function() {
-		expect(1);
-		var d = _add("d1");
-		m.on(d, "click", null);
-		ok(true, "bind with null function did not fail");
-	});
+    test("bind a null function", function () {
+        expect(1);
+        var d = _add("d1");
+        m.on(d, "click", null);
+        ok(true, "bind with null function did not fail");
+    });
 
     /**
      * Tests that unbinding a null function does not fail.
      * @method Mottle.Test.UnbindNull
      */
-	test("unbind a null function", function() {
-		expect(1);
-		var d = _add("d1");
-		m.on(d, "click", function() {});
-		m.off(d, "click", null);
-		ok(true, "unbind with null function did not fail");
-	});
+    test("unbind a null function", function () {
+        expect(1);
+        var d = _add("d1");
+        m.on(d, "click", function () {
+        });
+        m.off(d, "click", null);
+        ok(true, "unbind with null function did not fail");
+    });
 
     /**
      * Tests bind and remove functionality when using a dom element as argument.
      * @method Mottle.Test.BindRemoveWithElement
      */
-	test("bind and remove using Mottle, element as arg", function() {
+    test("bind and remove using Mottle, element as arg", function () {
         var d = _add("d1");
-		var a = 0;
-		m.on(d, "click", function() { a++; });
-		m.remove(d);
-		ok(d.parentNode == null, "div was removed from the dom");
-		m.trigger(d, "click");
-		ok(a == 0, "event was not fired");
+        var a = 0;
+        m.on(d, "click", function () {
+            a++;
+        });
+        m.remove(d);
+        ok(d.parentNode == null, "div was removed from the dom");
+        m.trigger(d, "click");
+        ok(a == 0, "event was not fired");
     });
 
     /**
-    * Tests bind and remove functionality when using a document query selector as argument.
-    * @method Mottle.Test.BindRemoveWithQuerySelector
-    */
-	test("bind and remove using Mottle, selector arg", function() {
+     * Tests bind and remove functionality when using a document query selector as argument.
+     * @method Mottle.Test.BindRemoveWithQuerySelector
+     */
+    test("bind and remove using Mottle, selector arg", function () {
         var d = _add("d1"), d2 = _add("d2");
-		var a = 0;
-		d.className = "foo";
-		d2.className = "foo";
-		var divs = document.querySelectorAll(".foo");
-		m.on(divs, "click", function() { a++; });
-		m.trigger(divs, "click");
-		ok(a == 2, "event was not fired");
-		m.remove(divs);
-		ok(d.parentNode == null, "div was removed from the dom");
-		ok(d2.parentNode == null, "div was removed from the dom");
-		m.trigger(divs, "click");
-		ok(a == 2, "event was not fired");
+        var a = 0;
+        d.className = "foo";
+        d2.className = "foo";
+        var divs = document.querySelectorAll(".foo");
+        m.on(divs, "click", function () {
+            a++;
+        });
+        m.trigger(divs, "click");
+        ok(a == 2, "event was not fired");
+        m.remove(divs);
+        ok(d.parentNode == null, "div was removed from the dom");
+        ok(d2.parentNode == null, "div was removed from the dom");
+        m.trigger(divs, "click");
+        ok(a == 2, "event was not fired");
     });
 
     /**
@@ -207,15 +231,32 @@ var testSuite = function() {
      * its mouse equivalent - `mousedown`.
      * @method Mottle.Test.ForceTouchEvents
      */
-    test("force touch events, bind to mousedown, and trigger a touchstart.", function() {
+    test("force touch events, bind to mousedown, and trigger a touchstart.", function () {
         Mottle.setForceTouchEvents(true);
         var d = _add("d1"), t = 0;
-        m.on(d, "mousedown", function() {
+        m.on(d, "mousedown", function () {
             t += 1;
         });
 
         m.trigger(d, "touchstart");
         equal(t, 1, "event was fired and captured");
+    });
+
+
+// -------------------- blur/focus -------------------------------------------
+
+    test("blur/focus events", function() {
+        var d = _add("d1"), d2 = _add("d2");
+        d2.setAttribute("tabindex", 1); //allow d2 to grab focus so that the blur gets fired.
+        var a = 0, b = 0;
+        m.on(d, "focus", function () { a++; });
+        m.on(d, "blur", function () {
+            b++;
+        });
+        d.focus();
+        equal(a, 1, "focus event captured");
+        d2.focus();
+        equal(b, 1, "blur event captured");
     });
 
     /**
@@ -229,7 +270,7 @@ var testSuite = function() {
      * within some proximity of the previous touch event.
      * @method Mottle.Test.TouchMouseDuplication
      *
-    test("filter duplicate mouse events.", function() {
+     test("filter duplicate mouse events.", function() {
         ok(true, "YOU MUST EITHER BE ON A TOUCH DEVICE OR EMULATE TOUCH EVENTS FOR THIS TEST TO WORK");
         Mottle.setForceTouchEvents(true);
         var d = _add("d1"), t = 0;
@@ -254,7 +295,6 @@ var testSuite = function() {
 
         equal(t, 1, "event was fired and captured only once.");
     });*/
-	
-	
-	
+
+
 };
